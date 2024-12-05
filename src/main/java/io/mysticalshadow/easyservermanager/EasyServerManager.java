39d9c32ffa266531e63f31c1cc96c1211805982a
MorphineDEV <69249795+Fukanoherde@users.noreply.github.com>
2024-12-05 03:32:30 +0100

    Create idea Project

---
+++ src/main/java/io/mysticalshadow/easyservermanager/EasyServerManager.java
@@ -0,0 +1,17 @@
+package io.mysticalshadow.easyservermanager;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class EasyServerManager extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
