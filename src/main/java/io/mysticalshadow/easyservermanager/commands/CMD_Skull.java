ed9952a2b361b52612be852db248adc0d5e0a9fb
Fukanoherde <69249795+Fukanoherde@users.noreply.github.com>
2024-12-14 15:04:24 +0100

    Update CMD_Skull.java

--- src/main/java/io/mysticalshadow/easyservermanager/commands/CMD_Skull.java
+++ src/main/java/io/mysticalshadow/easyservermanager/commands/CMD_Skull.java
@@ -1,10 +1,13 @@
 package io.mysticalshadow.easyservermanager.commands;
 
 import io.mysticalshadow.easyservermanager.EasyServerManager;
+import io.mysticalshadow.easyservermanager.api.ItemAPI;
 import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
 
 public class CMD_Skull implements CommandExecutor {
 
@@ -15,6 +18,29 @@ public class CMD_Skull implements CommandExecutor {
     }
     @Override
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
-        return false;
+        if (sender instanceof Player) {
+            Player p = (Player) sender;
+            if (p.hasPermission("")) {
+                if (args.length == 3) {
+                    String target = args[1];
+                    int amount = Integer.parseInt(args[2]);
+                    ItemStack playerHead = ItemAPI.createPlayerHead(target, target, amount, 0, "§2" + target);
+                    p.getInventory().addItem(playerHead);
+                    String givePlayerHead = plugin.YouGiveThePlayerHeadMSG;
+                    givePlayerHead = givePlayerHead.replace("%player%", target);
+                    p.sendMessage(plugin.Prefix + givePlayerHead);
+                    return true;
+                } else {
+                    p.sendMessage(plugin.Prefix + plugin.UseCommandMSG + "skull <player> <amount>");
+                    return true;
+                }
+            } else {
+                p.sendMessage(plugin.Prefix + plugin.NoPermMessage);
+                return true;
+            }
+        } else {
+            sender.sendMessage(plugin.Prefix + plugin.OnlyRealPlayer);
+            return true;
+        }
     }
 }
